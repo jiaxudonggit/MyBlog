23d3bdd7bf7d8fd8321b4205ecab7a90135bffea
jxd <403735024@qq.com>
2021-07-18 03:48:57 +0800

    3

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -5,7 +5,7 @@ module.exports = {
         exec_mode: 'cluster',
         instances: 'max', // Or a number of instances
         script: './node_modules/nuxt/bin/nuxt.js',
-        args: ['start', '-c ./nuxt.config.ts'],
+        args: ['start', '-c nuxt.config.ts'],
       }
     ]
   }
