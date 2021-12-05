a6af975dfd03871fe50df4e3ed2deb263c6eb171
jiaxudonggit <403735024@qq.com>
2021-12-05 21:28:50 +0800

    docker

--- blog.config.ts
+++ blog.config.ts
@@ -1,13 +1,13 @@
 import { IBlogConfig } from '@/types';
 
 const dbUser = 'blog';
-const dbPassword = 'Jxd970219@@';
+const dbPassword = 'Jxd970219';
 
 const config: IBlogConfig = {
   host: '0.0.0.0',
   port: 9000,
   enableHTTPS: false,
-  mongoUrl: `mongodb://${dbUser}:${dbPassword}@mongo:27017/blog?authSource=blog`,
+  mongoUrl: `mongodb://${dbUser}:${dbPassword}@${process.env.BUILD_ENV === 'docker' ? '49.232.124.18' : '49.232.124.18'}:27017/blog?authSource=admin`,
   jwtSecret: 'uN6wH5sY8iO0eM0pZ7wH2lC3oE8dU1zE'
 };
 
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -20,7 +20,7 @@ const config: Configuration = {
    ** Headers of the page
    */
   head: {
-    title: '莫愁前路',
+    title: 'iBlog',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
