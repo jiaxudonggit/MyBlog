a0d14bf669fae7ee0e6192809f356ce98774dd72
jiaxudonggit <403735024@qq.com>
2021-12-05 22:07:45 +0800

    docker

--- blog.config.ts
+++ blog.config.ts
@@ -2,12 +2,13 @@ import { IBlogConfig } from '@/types';
 
 const dbUser = 'blog';
 const dbPassword = 'Jxd970219';
+const host = process.env.BUILD_ENV === 'docker' ? 'mongo' : '49.232.124.18';
 
 const config: IBlogConfig = {
   host: '0.0.0.0',
   port: 9000,
   enableHTTPS: false,
-  mongoUrl: `mongodb://${dbUser}:${dbPassword}@${process.env.BUILD_ENV === 'docker' ? 'mongo' : '49.232.124.18'}:27017/blog?authSource=admin`,
+  mongoUrl: `mongodb://${dbUser}:${dbPassword}@${host}:27017/blog?authSource=admin`,
   jwtSecret: 'uN6wH5sY8iO0eM0pZ7wH2lC3oE8dU1zE'
 };
 
